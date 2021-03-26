f3934170b826797900c38b9ff5f063638e2357f9
Fatih Kartal <fatih2k16@hotmail.com>
2021-03-27 01:47:12 +0300

    GetAllTokensContinously

--- WEB/Scripts/Main.js
+++ WEB/Scripts/Main.js
@@ -1,13 +1,24 @@
 ﻿$(document).ready(function () {
-    GetAllTokens();
+    GetAllTokensContinuously();
 });
-
+function GetAllTokensContinuously() {
+    setInterval(GetAllTokens, 500);
+}
 function GetAllTokens() {
     $.ajax({
         url: "http://localhost:5000/api/Trade/GetAllTokens",
         type: "GET",
         success: function (response) {
-            console.log(data);
+            $("table tbody").empty()
+            for (var i = 0; i < response.length; i++) {
+                $("table tbody").append(
+                    `<tr>
+                        <td><span style="color:red">${response[i].first}</span> <span style="color:green">${response[i].second}</span></td >
+                        <td><span style="color:blue">${response[i].trades[0].price}</span></td >
+                    </tr>
+                    `
+                )
+            }
         }
     })
 }
