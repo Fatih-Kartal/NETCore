987e419fc7d7a0842f05ee81d71f9174ab662e0a
Fatih Kartal <fatih2k16@hotmail.com>
2021-03-27 01:19:26 +0300

    CORS settings and first request

--- NETCore Web Api/Startup.cs
+++ NETCore Web Api/Startup.cs
@@ -8,6 +8,7 @@ namespace NETCore_Web_Api
 {
     public class Startup
     {
+        readonly string AllowedSpecificOrigins = "_AllowedSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -19,24 +20,17 @@ namespace NETCore_Web_Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options => options.AddPolicy(name: AllowedSpecificOrigins, builder => builder.WithOrigins("http://localhost:5001").AllowAnyHeader().AllowAnyMethod()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
             app.UseRouting();
-
             app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseCors(AllowedSpecificOrigins);
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
         }
     }
 }
---
+++ WEB/Scripts/Main.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    GetAllTokens();
+});
+
+function GetAllTokens() {
+    $.ajax({
+        url: "http://localhost:5000/api/Trade/GetAllTokens",
+        type: "GET",
+        success: function (response) {
+            console.log(data);
+        }
+    })
+}
