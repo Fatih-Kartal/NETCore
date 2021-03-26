8ec9cdf755e217882acae891fd6fa7c0d5e15fd2
Fatih Kartal <fatih2k16@hotmail.com>
2021-03-26 19:57:30 +0300

    Add project files.

---
+++ NETCore Web Api/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NETCore_Web_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            return new { number = 1, letter = "apple" };
+        }
+    }
+}
